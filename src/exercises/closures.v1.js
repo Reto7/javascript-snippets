239d35c36e366b7f2b6b96196295abf101739834
Reto Kaufmann <reto.kaufmann@adon.li>
2017-04-18 19:07:24 +0200

    ,

---
+++ src/exercises/closures.v1.js
@@ -0,0 +1,15 @@
+// 1 
+// a. Was gibt folgender Code aus?
+
+for(var i=0;i<4;i++){
+    // todo als einfache funktion??
+    fn(zeit)
+    {
+        window.alert(i)
+    }
+    window.setTimeout(()=>{
+        f(i)
+    }, 2000)
+}
+
+// b. Wie kann ich das Programm ändern, damit es tut was es tun sollte.

05b8f9c1042529027d0c006df30cfcb56f995dc6
Reto Kaufmann <reto.kaufmann@adon.li>
2017-05-07 15:53:09 +0200

    .

--- src/exercises/iterator-v1.js
+++ src/exercises/iterator-v1.js
@@ -1,28 +1,52 @@
 // 1
 // Implementiere einen Array Iterator, der einen Array rückwärts interiert.
 // Versuche zwei Varianten zu finden.
-let arr = ['a','b','c'];
-
-// TODO  : soll mit  [Symbol.iterator](){} geloest werden!   -- siehe Beispiel!
+let meinArray = ['a','b','c'];
+console.log(meinArray)
 
 // 1-v1
 console.log('-1-v1-----------------------')
-for(let i=arr.length-1; i>=0; i--) {
-    console.log(arr[i])
+function reverseIterator1(arr){
+    this[Symbol.iterator] = ()=>{
+        let idx = arr.length
+        return {
+            next(){
+                return {
+                    done: --idx < 0,
+                    value: arr[idx]
+                }
+            }
+        }
+    }
 }
+//
+// NEIN so gehts nicht:
+// console.log(new reverseIterator1(meinArray))
+// SO GEHTS:
+// Statische factory Methode, um nicht new ReverseIterator… verwenden zu müssen.
+// Factory Methoden, wie .create, gelten als bessere Praxis, als new.
+reverseIterator1.create = (arr)=>{
+    return new reverseIterator1(arr)
+}
+//console.log(reverseIterator1.create(meinArray))
+for(let x of reverseIterator1.create(meinArray)){
+    console.log(x)
+}
+
+
+
+
 
 // 1-v2
 console.log('-1-v2-----------------------')
-function reverseIterator1(arr) {
+function reverseIterator2(arr) {
     return arr.reverse();
 }
-for (let x of reverseIterator1(arr) ) {
-    console.log(x)
-}
-// somit ok:
+console.log(reverseIterator2(meinArray))
 
 
-// 1-v3
+
+
 
 
 // 2 
@@ -32,20 +56,14 @@ for (let x of reverseIterator1(arr) ) {
 // entgegen nimmt.
 // Iteriere über das iterable Objekt und rufe den Callback für jeden Wert auf, 
 // solange bis alle Elemente iteriert sind.
-
+//
 console.log('-2-v1-----------------------')
-// 1-v2
-// class Klasse {
-//     constructor(myArray) {
-//         this.myArray = myArray
-//     }
-//
-//     [Symbol.iterator](){}
-//
-// }
-//
-// let k1 = new Klasse(arr)
-// console.log(arr.length)
-// for (let x of arr){
-//     console.log(x)
-// }
+function forFun(iteratable, forEach){
+    let it = iteratable[Symbol.iterator](),
+        item,
+        idx = 0
+    while((item = it.next()).done == false){
+        forEach(item.value, idx++)
+    }
+}
+forFun(new reverseIterator1([1,2,3]), console.log)  // TODO Bahnhof.... console.log ist die callback methode... ?
